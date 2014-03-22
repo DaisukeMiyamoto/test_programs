a2b7dd6ab44dd4ea7ef2b0e747d94f52f49cc9f7
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2014-03-22 20:30:15 +0900

    add test program for vdExp() from intel MKL examples

---
+++ test_vdexp/func_interv.h
@@ -0,0 +1,190 @@
+/*******************************************************************************
+!                             INTEL CONFIDENTIAL
+!   Copyright(C) 2001-2004 Intel Corporation. All Rights Reserved.
+!   The source code contained  or  described herein and all documents related to
+!   the source code ("Material") are owned by Intel Corporation or its suppliers
+!   or licensors.  Title to the  Material remains with  Intel Corporation or its
+!   suppliers and licensors. The Material contains trade secrets and proprietary
+!   and  confidential  information of  Intel or its suppliers and licensors. The
+!   Material  is  protected  by  worldwide  copyright  and trade secret laws and
+!   treaty  provisions. No part of the Material may be used, copied, reproduced,
+!   modified, published, uploaded, posted, transmitted, distributed or disclosed
+!   in any way without Intel's prior express written permission.
+!   No license  under any  patent, copyright, trade secret or other intellectual
+!   property right is granted to or conferred upon you by disclosure or delivery
+!   of the Materials,  either expressly, by implication, inducement, estoppel or
+!   otherwise.  Any  license  under  such  intellectual property  rights must be
+!   express and approved by Intel in writing.
+!
+!*******************************************************************************
+!  Content:
+!      Example Program Text
+!******************************************************************************/
+
+#define VEC_LEN 11
+
+#define __SINV_BEG -10000.0
+#define __SINV_END 10000.0
+
+#define __SDIV_BEG -10000.0
+#define __SDIV_END 10000.0
+
+#define __SSQRT_BEG 0.0
+#define __SSQRT_END 10000.0
+
+#define __SINVSQRT_BEG 0.1
+#define __SINVSQRT_END 10000.0
+
+#define __SPOW_BEG 0.1
+#define __SPOW_END 7.0
+
+#define __SPOWX_BEG 0.1
+#define __SPOWX_END 7.0
+
+#define __SERF_BEG 1.0
+#define __SERF_END 4.0
+
+#define __SERFC_BEG 1.0
+#define __SERFC_END 4.0
+
+#define __SEXP_BEG -17.0
+#define __SEXP_END 18.0
+
+#define __SLN_BEG 0.1
+#define __SLN_END 10000.0
+
+#define __SLOG10_BEG 0.1
+#define __SLOG10_END 10000.0
+
+#define __SCOS_BEG -10000.0
+#define __SCOS_END 10000.0
+
+#define __SSIN_BEG -10000.0
+#define __SSIN_END 10000.0
+
+#define __SSINCOS_BEG -10000.0
+#define __SSINCOS_END 10000.0
+
+#define __STAN_BEG -10000.0
+#define __STAN_END 10000.0
+
+#define __SACOS_BEG -0.9
+#define __SACOS_END +0.9
+
+#define __SASIN_BEG -1.0
+#define __SASIN_END +1.0
+
+#define __SATAN_BEG -10000.0
+#define __SATAN_END 10000.0
+
+#define __SATAN2_BEG -10000.0
+#define __SATAN2_END 10000.0
+
+#define __SCOSH_BEG -7.0
+#define __SCOSH_END 7.0
+
+#define __SSINH_BEG -7.0
+#define __SSINH_END 7.0
+
+#define __STANH_BEG -10.0
+#define __STANH_END 10.0
+
+#define __SACOSH_BEG 3.5
+#define __SACOSH_END 10000.0
+
+#define __SASINH_BEG 3.0
+#define __SASINH_END 10000.0
+
+#define __SATANH_BEG 0.75
+#define __SATANH_END 0.9
+
+#define __SCBRT_BEG 0.0
+#define __SCBRT_END 10000.0
+
+#define __SINVCBRT_BEG 0.1
+#define __SINVCBRT_END 10000.0
+
+#define __SGAMMA_BEG -500.0
+#define __SGAMMA_END +500.0
+
+#define __DINV_BEG -10000.0
+#define __DINV_END 10000.0
+
+#define __DDIV_BEG -100.0
+#define __DDIV_END 100.0
+
+#define __DSQRT_BEG 0.0
+#define __DSQRT_END 10000.0
+
+#define __DINVSQRT_BEG 0.1
+#define __DINVSQRT_END 10000.0
+
+#define __DPOW_BEG 0.1
+#define __DPOW_END 7.0
+
+#define __DPOWX_BEG 0.1
+#define __DPOWX_END 7.0
+
+#define __DERF_BEG 0.5
+#define __DERF_END 5.9
+
+#define __DERFC_BEG 0.5
+#define __DERFC_END 5.9
+
+#define __DEXP_BEG -17.0
+#define __DEXP_END 17.0
+
+#define __DLN_BEG 0.1
+#define __DLN_END 10000.0
+
+#define __DLOG10_BEG 0.1
+#define __DLOG10_END 10000.0
+
+#define __DCOS_BEG -10000.0
+#define __DCOS_END 10000.0
+
+#define __DSIN_BEG -10000.0
+#define __DSIN_END 10000.0
+
+#define __DSINCOS_BEG -10000.0
+#define __DSINCOS_END 10000.0
+
+#define __DTAN_BEG -10000.0
+#define __DTAN_END 10000.0
+
+#define __DACOS_BEG -0.9
+#define __DACOS_END +0.9
+
+#define __DASIN_BEG -1.0
+#define __DASIN_END +1.0
+
+#define __DATAN_BEG -10000.0
+#define __DATAN_END +10000.0
+
+#define __DATAN2_BEG -10000.0
+#define __DATAN2_END +10000.0
+
+#define __DCOSH_BEG -7.0
+#define __DCOSH_END 7.0
+
+#define __DSINH_BEG -7.0
+#define __DSINH_END 7.0
+
+#define __DTANH_BEG -20.0
+#define __DTANH_END 20.0
+
+#define __DACOSH_BEG 1.001
+#define __DACOSH_END 10000.0
+
+#define __DASINH_BEG 0.002
+#define __DASINH_END 10000.0
+
+#define __DATANH_BEG 0.001
+#define __DATANH_END 0.9
+
+#define __DCBRT_BEG 0.0
+#define __DCBRT_END 10000.0
+
+#define __DINVCBRT_BEG 0.1
+#define __DINVCBRT_END 10000.0
+
